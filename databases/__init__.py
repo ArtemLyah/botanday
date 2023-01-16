d63c35fb32160400d5bd4cfa0340a60fefcd92b6
ArtemLyah <asuskonotopo@gmail.com>
2023-01-17 00:29:47 +0200

    create tables in db

--- databases/__init__.py
+++ databases/__init__.py
@@ -3,4 +3,4 @@ from .models import Groups, Users, UserGroupStats, BotanInfo
 import sqlalchemy as sa
 from sqlalchemy.sql.selectable import Select
 
-# Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
