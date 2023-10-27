f0a20fd3f136b1a3eb75bd567c1781c09d7b2947
ArtemLyah <asuskonotopo@gmail.com>
2023-10-27 21:10:34 +0300

    debug database connection

--- databases/connection.py
+++ databases/connection.py
@@ -6,7 +6,13 @@ from config import database_settings
 class Database():
     url = sa.engine.url.URL.create(**database_settings)
 
-    engine = sa.create_engine(url)
+    engine = sa.create_engine(url, 
+        pool_size=10,
+        max_overflow=2,
+        pool_recycle=300,
+        pool_pre_ping=True,
+        pool_use_lifo=True
+    )
     Base = declarative_base()
     
     @classmethod
